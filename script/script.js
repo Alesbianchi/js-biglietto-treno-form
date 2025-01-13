00adf95dd88124bfedb037834766c7e971003281
Alessandro Bianchi <ale.bianchi22@hotmail.it>
2025-01-13 16:18:25 +0100

    restart from logic

--- script/script.js
+++ script/script.js
@@ -4,3 +4,19 @@
 // - va applicato uno sconto del 40% per gli over 65.
 // MILESTONE 1:
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+
+// MILESTONE 2:
+// Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form completo in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo. Il recap dei dati e l’output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo).
+
+
+
+// INPUT
+
+// l'utente inserisce la sua età
+
+
+// l'utente inserisce il numero di km
+
+// il PC restituisce un numero
+
+// ELABORAZIONE
